b641ca1edd8eb38c75abb92c144bd36a3fc47728
Maryanemwende <maryanemwende333@gmail.com>
2023-03-16 01:03:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle\n");
+#include <stdio.h>
+
+/**
+ * main - This prints Hello puts
+ * Return : This returns 0
+ */
+
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
