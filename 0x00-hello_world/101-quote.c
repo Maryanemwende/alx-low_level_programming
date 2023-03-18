4789cc497f2f46aab3cf48fe429afc9d73b42ce8
Maryanemwende <maryanemwende333@gmail.com>
2023-03-17 17:56:18 -0700

    2-print_alphabet.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  * Return: 1 (success)
  */
 int main(void)
-{
-	write('"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+({
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 41);
 	return (1);
-}
+)}
