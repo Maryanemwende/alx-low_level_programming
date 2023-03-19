26001ff28514a4920ecd5c844c514a555e649249
Maryanemwende <maryanemwende333@gmail.com>
2023-03-19 14:29:52 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,13 @@
 /**
  * main - Entry point
  *
- * Return: 1 (success)
+ * Return: 1
  */
 int main(void)
-({
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 41);
+{
+	write(1,
+		"and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n",
+		99);
+
 	return (1);
-)}
+}
