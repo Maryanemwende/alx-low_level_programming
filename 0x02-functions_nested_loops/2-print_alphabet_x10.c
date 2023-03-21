4b4849156b47f9ce5b72a7454750f00bb9d5d3d5
Maryanemwende <maryanemwende333@gmail.com>
2023-03-21 10:11:33 -0700

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,15 @@
 void print_alphabet_x10(void)
 {
 	int lines = 0;
-	char alp = 'a';
+	char alp;
 
 	while (lines < 10)
 	{
-		for (alp = 'a'; alp <= 'z'; alp++)
+		alp = 'a';
+		while (alp <= 'z')
 		{
 			_putchar(alp);
+			alp++;
 		}
 		lines++;
 		_putchar('\n');
