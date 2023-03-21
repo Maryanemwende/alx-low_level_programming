47735b4d7f11304d87c9898ef994b20f387d78ab
Maryanemwende <maryanemwende333@gmail.com>
2023-03-21 16:19:29 -0700

    8-24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
+ *
+ * Return: minute of the day
+ */
+void jack_bauer(void)
+{
+	int hr = 0;
+	int min;
+
+	for (; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
