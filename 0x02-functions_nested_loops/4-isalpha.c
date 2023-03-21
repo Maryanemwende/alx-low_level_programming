e8ca336bf5d9726af8eb7e4ba87f71a13402eb97
Maryanemwende <maryanemwende333@gmail.com>
2023-03-21 13:18:32 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,11 +9,17 @@
 int _isalpha(int c)
 {
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else if (c >= 'a' && c <= 'z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 
 	_putchar('\n');
 }
