1a3895d0cd9c3265c98e0e164638446c880844df
Maryanemwende <maryanemwende333@gmail.com>
2023-03-21 18:45:31 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table
+ * starting with 0
+ *
+ * @n: value of timetable to be printed
+ * Return: 0 (Successful)
+ */
+void print_times_table(int n)
+{
+	int timesTable;
+
+	timeTable = n;
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - list all the natural numbers below 10 that are multiples of 3 or 5
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int x;
+	int sum;
+
+	sum = 0;
+
+	for (x = 0; x < 1024; x++)
+	{
+		if ((x % 3) == 0 || (x % 5) == 0)
+
+			sum += x;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
