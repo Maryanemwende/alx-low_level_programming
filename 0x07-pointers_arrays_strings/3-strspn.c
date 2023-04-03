f2341790c1fa164c587a29941daf66c7918b8f6a
Maryanemwende <maryanemwende333@gmail.com>
2023-04-03 16:27:11 -0700

    3-strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ *
+ * @s: string to be checked
+ * @accept: prefix substring to be measured
+ * Return: only bytes in s with bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int total = 0;
+	int bytes = 0;
+	int length = 0;
+
+	for (bytes = 0; s[bytes] != '\0'; bytes++)
+	{
+		for (length = 0; accept[length] != '\0'; length++)
+		{
+			if (s[bytes] == accept[length])
+				total++;
+			break;
+		}
+		if (accept[length] == '\0')
+			return (total);
+	}
+	return (total);
+}
