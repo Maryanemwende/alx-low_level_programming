9452bc99409f46179c93682a33a4ef3d73f38ead
Maryanemwende <maryanemwende333@gmail.com>
2023-04-10 06:48:40 -0700

    mynewnameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - takes CL arguments
+ *
+ * @argc: number of arguments passed to program
+ * @argv: arrayr of strings containing the actual arguments
+ * Return: 0 (Successful)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
