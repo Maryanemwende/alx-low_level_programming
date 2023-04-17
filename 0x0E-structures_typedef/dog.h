27f98273b542ba9db38f24f34f444cbbccbf5264
Maryanemwende <maryanemwende333@gmail.com>
2023-04-17 07:39:52 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type struct dog
+ *
+ * @name: input name
+ * @age: input age
+ * @owner: input owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif/*DOG_H*/
