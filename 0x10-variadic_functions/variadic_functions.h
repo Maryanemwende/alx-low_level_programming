2ff05f9f1b252613859cc94c81c2402bcde0f0ed
Maryanemwende <maryanemwende333@gmail.com>
2023-04-20 11:49:23 -0700

    a

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-/**
- * Header file - contains all my function prototypes
- *
- */
-
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
