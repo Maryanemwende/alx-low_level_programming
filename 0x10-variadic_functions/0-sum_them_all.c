0a445043bd7449cba0a6004bb0ebb24cf5a53ee3
Maryanemwende <maryanemwende333@gmail.com>
2023-04-20 09:59:07 -0700

    a

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,10 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_list args;
 
+	va_start(args, n);
+
 	if (n == 0)
 		return (0);
-
-	va_start(args, n);
 
 	for (elem = 0; elem < n; elem++)
 	{
