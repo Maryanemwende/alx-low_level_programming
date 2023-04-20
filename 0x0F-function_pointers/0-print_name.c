c79341d135e4a219df8be8297989d9486fdf6f66
Maryanemwende <maryanemwende333@gmail.com>
2023-04-19 18:53:41 -0700

    a

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints a name
@@ -8,7 +9,11 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
+	{
 		return;
-
-	(*f)(name);
+	}
+	else
+	{
+		(*f)(name);
+	}
 }
