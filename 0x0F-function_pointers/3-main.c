0a143816933c6a6495e9a0ef87f0d17d54f10626
Maryanemwende <maryanemwende333@gmail.com>
2023-04-19 17:09:47 -0700

    calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,7 +13,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1;
 	int num2;
-	char *op_func;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -22,27 +22,27 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	op_func = argv[2];
+	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (get_op_func(op_func) == NULL || op_func[1] != '\0')
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if (*op_func == '/' && num2 == 0)
+	if (*op == '/' && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	if (*op_func == '%' && num2 == 0)
+	if (*op == '%' && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(op_func)(num1, num2));
+	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
 }
