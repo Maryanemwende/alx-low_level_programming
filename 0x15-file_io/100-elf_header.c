8a63437c4e197f2f9b078114f68210fa958f2612
Maryanemwende <maryanemwende333@gmail.com>
2023-05-26 19:02:01 -0700

    100-elf_header.c

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,19 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: array of pointers to arguments
+ * Return: 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+
+	return (0);
+}
